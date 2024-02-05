1fc2d977f9f48ca31ea429bbe74ea69c5dba70b8
cloud <313672099@qq.com>
2024-02-05 20:35:36 +0800

    添加课程相关资料，修改头文件注释

--- scan.h
+++ scan.h
@@ -28,9 +28,9 @@ public:
 	node* parent = NULL;//父母结点
 
 	node() = default;//默认构造函数
-	node(string name, time_t time, int size, node* parent);//初始化结点
-	void print_node();//打印信息
-	bool operator== (const node& other);
+	node(string name, time_t time, int size, node* parent);//初始化结点，输入路径、文件修改时间、文件大、父母结点
+	void print_node();//打印结点信息
+	bool operator== (const node& other);//重载运算符==，方便结点间进行比较
 };
 
 
@@ -44,30 +44,30 @@ public:
 	int num_total = 0;//文件总数
 	_fsize_t size_total = 0;//文件总大小
 
-	statInfo(string path);
+	statInfo(string path);//构造函数，输入路径初始化类成员
 	void print_info();//打印信息
-	string statusInfo();//将统计状态转换为文字
+	string statusInfo();//将统计状态转换为文字，调用status，返回字符串
 };
 
 class dir_tree//目录树
 {
 public:
-	node* root = NULL;//根目录
+	node* root = NULL;//根目录结点指针
 	int depth = 0;//树的深度
 
-	void create(string searchpath, string sqlPath);//创建树
-	int destroy(node* p);//删除结点p及其子结点, 返回删除结点数
-	int cal_depth();//计算树的深度
+	void create(string searchpath, string sqlPath);//创建树，输入扫描目录和sql文件生成路径
+	int destroy(node* p);//输入指向结点的指针p，删除结点及其子结点, 返回删除结点数
+	int cal_depth();//计算并返回树的深度
 
-	node* statNode(node* p, statInfo& info);//对指定结点进行统计
-	node* rstat(string path);//统计指定目录及其子目录，并打印信息，返回统计对象指针
-	bool mul_stat(string filePath, vector<statInfo>& infoSet);//依据指定文件，批量统计目录，结果存于infoSet
-	node* find(string path);//寻找目录或文件
-	void cmpStat(vector<statInfo> infoSet1, vector<statInfo> infoSet2);//对统计信息进行比较
+	node* statNode(node* p, statInfo& info);//对指定结点进行统计，输入结点指针p和统计信息，将统计结果保存在info中，返回p
+	node* rstat(string path);//统计指定目录及其子目录，并打印信息，输入路径，返回对应结点指针，不存在则返回NULL
+	bool mul_stat(string filePath, vector<statInfo>& infoSet);//依据指定文件，批量统计目录，结果存于infoSet，统计成功返回true，失败返回false
+	node* find(string path);//寻找目录或文件，输入路径，返回对应结点指针，不存在则返回NULL
+	void cmpStat(vector<statInfo> infoSet1, vector<statInfo> infoSet2);//输入两个统计信息，对其进行比较，输出差异
 
-	void op_file(string opStatement);//对指定文件进行操作
-	void op_dir(string opStatement);//对指定目录进行操作，返回操作的结点数
-	void mul_op_file(string filePath);//批量进行文件操作
-	void mul_op_dir(string filePath);//批量进行目录操作
-	void opsplit(string opStatement, string& path, string& opcode, time_t& time, _fsize_t& size);//分离操作参数
+	void op_file(string opStatement);//对指定文件进行操作，输入操作指令
+	void op_dir(string opStatement);//对指定目录进行操作，输入操作指令
+	void mul_op_file(string filePath);//批量进行文件操作，输入批量操作文件路径
+	void mul_op_dir(string filePath);//批量进行目录操作，输入批量操作文件路径
+	void opsplit(string opStatement, string& path, string& opcode, time_t& time, _fsize_t& size);//分离操作参数，输入操作指令，各部分存储于path、opcode、time、size
 };
